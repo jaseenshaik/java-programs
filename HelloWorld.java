183323997b89a0af5e52bde05252aa21dff39a84
Jaseen Shaik <jaseenshaik@gmail.com>
2026-02-10 19:17:18 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Jaseen Shaik!");
+        System.out.println("Starting my Java and IT journey.");
+    }
+}
