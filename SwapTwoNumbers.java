15d1d7d7b7a57429b57321668e8c379348af1ad0
Jaseen Shaik <jaseenshaik@gmail.com>
2026-02-10 19:30:29 +0530

    Added swap two numbers using temp variable

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+
+        // Swapping using a temporary variable
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+
+        sc.close();
+    }
+}
